c8fe13210fb76c58fcf9e2a1dab0050fa96e1ce5
dsplayer97 <18813060311@163.com>
2018-03-23 18:12:12 +0800

    texture and ball control
    a lots of texture for ball and cube, a control script for ball

---
+++ Mission/Assets/script/control.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class control : MonoBehaviour
+{
+
+    // Use this for initialization
+    private float x, y, z;
+
+    public Rigidbody squ;
+
+
+    void Start()
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {   x = Input.acceleration.x*10;
+        y = Input.acceleration.y*10;
+       /* speed = Mathf.Sqrt(x * x + y * y);
+        //Vector3.right x zhou  
+        horizontalMovement = Input.GetAxis("Horizontal") * Vector3.right * speed;
+
+        //z zhou  
+        verticalMovement = Input.GetAxis("Vertical") * Vector3.forward * speed;
+
+        //小球的运动  
+        Vector3 movement = horizontalMovement + verticalMovement;*/
+
+        squ.AddForce(new Vector3(x, 0, y), ForceMode.Force);
+    
+       
+  
+        //z = Input.acceleration.z / 5;
+        /*
+        if (x < 0.5)
+        {
+            x = 0;
+        }
+        if (y < 0.5)
+        {
+            y = 0;
+        }*/
+
+        
+
+
+
+
+    }
+    
+}
